95f19abad0f293e6f25e4865090479394ddd2b7b
isabeladommus <isabela.aquino@dommus.com.br>
2022-03-14 19:53:41 -0300

    primeiro commit

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import { Switch, Route } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+
+
+export default function Routes() {
+  return (
+    <Switch>
+        {/* <Route path="/login" exact component={Logout} />
+        <Route path="/sair" exact component={Logout} />
+        <Route path="/gu-login/:token" exact component={GuLogin} /> */}
+    </Switch>
+  )
+}
