072343f5577368ba61f13d9f88ba3d145e4e5590
Jonas-Alvess <jonas.alves2011@hotmail.com>
2022-04-27 21:33:43 -0300

    up

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,26 +1,51 @@
 import React, { useState } from 'react';
 import Contato from '../Contato';
 import { Link } from 'react-router-dom';
+import './home.css';
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExpand } from "@fortawesome/free-solid-svg-icons"; 
+import Card from "../../componentes/card"
+import dados from "../../dados/dados.json"
 
 
 
 // import { Container } from './styles';
 
+
 function Home() {
 
   const [visivel, setvisivel] = useState(false)
+  const handle = useFullScreenHandle();
+  let title ="Jonas";
 
   function mudarTela(e){
     e.preventDefault()
     setvisivel(true)
   }
+  
   return (<div className='home'>
-      <h1>Boa noite!</h1>
-      {/* {
-        visivel ? <Contato></Contato> : <></>
-      } */}
+
+  <FullScreen className='videof' handle={handle}>
+
+  <iframe  className='video' src="https://www.youtube.com/embed/jukLsFPRBWA" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+
+  </FullScreen>
+
+  <div className='exp'
+    onClick={handle.enter}
+    ata-tip="FullScreen"><FontAwesomeIcon color='#FF0000' icon={faExpand}
+     />
+  </div>
+  <div className="estilo-card">
+  {
+    dados.map((dado, i)=>(
+      <Card key={i} title={dado.titulo}/>
+    ))
+  }
+  </div>
+  
       
-      <button className="btn btn-primary"><Link to={"/contato"}>Contatos</Link></button>
   </div>)
 }
 
